01cc0735ac317397c3f26a6b8e896a94dc240574
Joshiel Cacciavillani <nicolasjoshiel@gmail.com>
2022-05-17 23:02:06 -0300

    Add NavBar and Material UI

--- cryptomarket/src/components/NavBar/NavBar.js
+++ cryptomarket/src/components/NavBar/NavBar.js
@@ -1,6 +1,23 @@
+import "./NavBar.css";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
 //los componentes se llaman Upper CammelCase
 const NavBar = () => {
-  return <div className='navBar'> Esto es un NavBar o barra de Navegación</div>;
+  return (
+    <AppBar position='static'>
+      <Toolbar style={{ backgroundColor: "#3949ab" }}>
+        <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+          CryptoMarket
+        </Typography>
+        <Button color='inherit'>Home</Button>
+        <Button color='inherit'>PortFolio</Button>
+        <Button color='inherit'>Contact</Button>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default NavBar;
